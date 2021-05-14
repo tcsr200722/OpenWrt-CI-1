a833069f83019ac8f90a829f02486895a594a8bb
Bob Zhang <zhang.deplives@gmail.com>
2021-05-14 10:26:28 +0800

    delete feed.conf.default

--- feeds.sh
+++ feeds.sh
@@ -2,7 +2,8 @@
 # https://github.com/deplives/OpenWrt-CI/
 
 # Uncomment a feed source
-# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+echo 'Uncomment helloworld feed...'
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
