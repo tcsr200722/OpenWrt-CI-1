52cec4c78ef8c87e3dd610459727a7e0cde72c7a
Bob Zhang <zhang.deplives@gmail.com>
2021-07-01 10:42:42 +0800

    banner新增commit hash

--- config.sh
+++ config.sh
@@ -2,11 +2,11 @@
 # https://github.com/deplives/OpenWrt-CI
 
 # Modify default IP
-# sed -i "s/192.168.1.1/192.168.50.2/g" package/base-files/files/bin/config_generate
+# sed -i 's/192.168.1.1/192.168.50.2/g' package/base-files/files/bin/config_generate
 
 # Modify default theme
 echo "Modify default theme..."
-sed -i "s/luci-theme-bootstrap/luci-theme-argon/g" feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Modify default luci-app-samba
 # https://github.com/deplives/luci-app-samba
@@ -39,43 +39,43 @@ rm -rf package/lean/luci-app-ttyd && git clone https://github.com/deplives/luci-
 
 # Modify default NTP server
 echo "Modify default NTP server..."
-sed -i "s/ntp1.aliyun.com/ntp.ntsc.ac.cn/g" package/base-files/files/bin/config_generate
-sed -i "s/time1.cloud.tencent.com/ntp.aliyun.com/g" package/base-files/files/bin/config_generate
-sed -i "s/time.ustc.edu.cn/time.google.com/g" package/base-files/files/bin/config_generate
-sed -i "s/cn.pool.ntp.org/pool.ntp.org/g" package/base-files/files/bin/config_generate
+sed -i 's/ntp1.aliyun.com/ntp.ntsc.ac.cn/g' package/base-files/files/bin/config_generate
+sed -i 's/time1.cloud.tencent.com/ntp.aliyun.com/g' package/base-files/files/bin/config_generate
+sed -i 's/time.ustc.edu.cn/time.google.com/g' package/base-files/files/bin/config_generate
+sed -i 's/cn.pool.ntp.org/pool.ntp.org/g' package/base-files/files/bin/config_generate
 
 # Modify default timezone
 echo "Modify default timezone..."
-sed -i "s/UTC/Asia\/Shanghai/g" package/base-files/files/bin/config_generate
+sed -i 's/UTC/Asia\/Shanghai/g' package/base-files/files/bin/config_generate
 
 # Modify zzz-default-settings
-echo "delete `sed -i "s#downloads.openwrt.org#mirrors.cloud.tencent.com/lede#g" /etc/opkg/distfeeds.conf`"
-sed -i "/mirrors.cloud.tencent.com/d" package/lean/default-settings/files/zzz-default-settings
+echo "delete `sed -i 's#downloads.openwrt.org#mirrors.cloud.tencent.com/lede#g' /etc/opkg/distfeeds.conf`"
+sed -i '/mirrors.cloud.tencent.com/d' package/lean/default-settings/files/zzz-default-settings
 
-echo "delete `sed -i "s/\"services\"/\"nas\"/g" /usr/lib/lua/luci/controller/hd_idle.lua`"
-sed -i "/hd_idle.lua/d" package/lean/default-settings/files/zzz-default-settings
+echo "delete `sed -i 's/\"services\"/\"nas\"/g' /usr/lib/lua/luci/controller/hd_idle.lua`"
+sed -i '/hd_idle.lua/d' package/lean/default-settings/files/zzz-default-settings
 
-echo "delete `sed -i "s/\"services\"/\"nas\"/g" /usr/lib/lua/luci/controller/samba.lua`"
-sed -i "/samba.lua/d" package/lean/default-settings/files/zzz-default-settings
+echo "delete `sed -i 's/\"services\"/\"nas\"/g' /usr/lib/lua/luci/controller/samba.lua`"
+sed -i '/samba.lua/d' package/lean/default-settings/files/zzz-default-settings
 
 # Modify some html style
 # Modify admin/status/overview <td id="wan4_i" style="width:16px; text-align:center; padding:3px">
 echo "Modify admin/status/overview <td id="wan4_i" style="width:16px; text-align:center; padding:3px">"
-sed -i "s/<td id="wan4_i" style="width:16px; text-align:center; padding:3px">/<td id="wan4_i" style="width:10%; text-align:center; padding:3px">/g" feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
-sed -i "s/<td id="wan4_i" style="width:16px; text-align:center; padding:3px">/<td id="wan4_i" style="width:10%; text-align:center; padding:3px">/g" package/lean/autocore/files/x86/index.htm
-sed -i "s/<td id="wan4_i" style="width:16px; text-align:center; padding:3px">/<td id="wan4_i" style="width:10%; text-align:center; padding:3px">/g" package/lean/autocore/files/arm/index.htm
+sed -i 's/<td id="wan4_i" style="width:16px; text-align:center; padding:3px">/<td id="wan4_i" style="width:10%; text-align:center; padding:3px">/g' feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
+sed -i 's/<td id="wan4_i" style="width:16px; text-align:center; padding:3px">/<td id="wan4_i" style="width:10%; text-align:center; padding:3px">/g' package/lean/autocore/files/x86/index.htm
+sed -i 's/<td id="wan4_i" style="width:16px; text-align:center; padding:3px">/<td id="wan4_i" style="width:10%; text-align:center; padding:3px">/g' package/lean/autocore/files/arm/index.htm
 
 # Modify admin/status/overview <td id="wan6_i" style="width:16px; text-align:center; padding:3px">
 echo "Modify admin/status/overview <td id="wan6_i" style="width:16px; text-align:center; padding:3px">"
-sed -i "s/<td id="wan6_i" style="width:16px; text-align:center; padding:3px">/<td id="wan6_i" style="width:10%; text-align:center; padding:3px">/g" feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
-sed -i "s/<td id="wan6_i" style="width:16px; text-align:center; padding:3px">/<td id="wan6_i" style="width:10%; text-align:center; padding:3px">/g" package/lean/autocore/files/x86/index.htm
-sed -i "s/<td id="wan6_i" style="width:16px; text-align:center; padding:3px">/<td id="wan6_i" style="width:10%; text-align:center; padding:3px">/g" package/lean/autocore/files/arm/index.htm
+sed -i 's/<td id="wan6_i" style="width:16px; text-align:center; padding:3px">/<td id="wan6_i" style="width:10%; text-align:center; padding:3px">/g' feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
+sed -i 's/<td id="wan6_i" style="width:16px; text-align:center; padding:3px">/<td id="wan6_i" style="width:10%; text-align:center; padding:3px">/g' package/lean/autocore/files/x86/index.htm
+sed -i 's/<td id="wan6_i" style="width:16px; text-align:center; padding:3px">/<td id="wan6_i" style="width:10%; text-align:center; padding:3px">/g' package/lean/autocore/files/arm/index.htm
 
 # Modify admin/status/overview <td id="dsl_i" style="width:16px; text-align:center; padding:3px">
 echo "Modify admin/status/overview <td id="dsl_i" style="width:16px; text-align:center; padding:3px">"
-sed -i "s/<td id="dsl_i" style="width:16px; text-align:center; padding:3px">/<td id="dsl_i" style="width:10%; text-align:center; padding:3px">/g" feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
-sed -i "s/<td id="dsl_i" style="width:16px; text-align:center; padding:3px">/<td id="dsl_i" style="width:10%; text-align:center; padding:3px">/g" package/lean/autocore/files/x86/index.htm
-sed -i "s/<td id="dsl_i" style="width:16px; text-align:center; padding:3px">/<td id="dsl_i" style="width:10%; text-align:center; padding:3px">/g" package/lean/autocore/files/arm/index.htm
+sed -i 's/<td id="dsl_i" style="width:16px; text-align:center; padding:3px">/<td id="dsl_i" style="width:10%; text-align:center; padding:3px">/g' feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
+sed -i 's/<td id="dsl_i" style="width:16px; text-align:center; padding:3px">/<td id="dsl_i" style="width:10%; text-align:center; padding:3px">/g' package/lean/autocore/files/x86/index.htm
+sed -i 's/<td id="dsl_i" style="width:16px; text-align:center; padding:3px">/<td id="dsl_i" style="width:10%; text-align:center; padding:3px">/g' package/lean/autocore/files/arm/index.htm
 
 # Modify default banner
 echo "Modify default banner..."
